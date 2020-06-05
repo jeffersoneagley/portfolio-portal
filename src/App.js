7cb07ff24d7d7a4766b3fb8acab17d066199e0d5
Ilithor <daryk.kohler@gmail.com>
2020-06-05 09:40:22 -0700

    Setup navigate buttons, convert to es6

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Stack, IStackTokens, PrimaryButton } from "office-ui-fabric-react";
 
-function App() {
+export const App = () => {
+  const stackTokens: IStackTokens = { childrenGap: 20 };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
+      <h2>Daryk Kohler</h2>
+      <Stack horizontalAlign="center" tokens={stackTokens}>
+        <PrimaryButton
+          href="https://socmon.azurewebsites.net/"
           target="_blank"
-          rel="noopener noreferrer"
+          iconProps={{ iconName: "RedEye" }}
         >
-          Learn React
-        </a>
-      </header>
+          SocMon
+        </PrimaryButton>
+        <PrimaryButton
+          href="https://www.linkedin.com/in/daryk-kohler/"
+          target="_blank"
+          iconProps={{ iconName: "LinkedInLogo" }}
+        >
+          LinkedIn
+        </PrimaryButton>
+        <PrimaryButton
+          href="https://github.com/Ilithor"
+          target="_blank"
+          iconProps={{ iconName: "Database" }}
+        >
+          GitHub
+        </PrimaryButton>
+      </Stack>
     </div>
   );
-}
-
-export default App;
+};
