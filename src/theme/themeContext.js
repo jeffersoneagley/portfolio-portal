a15b7378933c12820a6fce7695bb38e7929a6543
Ilithor <daryk.kohler@gmail.com>
2020-06-06 20:56:45 -0700

    Setup theme context to handle dark/light theme

---
+++ src/theme/themeContext.js
@@ -0,0 +1,40 @@
+import React, { createContext, useContext, useState } from "react";
+import { darkCustomizations, lightCustomizations } from "./customizations";
+import { Customizer } from "@fluentui/react";
+
+const themeContext = createContext();
+
+export const ThemeProvider = ({ children }) => {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+  const customizations = isDarkMode ? darkCustomizations : lightCustomizations;
+  const changeTheme = () => setIsDarkMode(!isDarkMode);
+
+  return (
+    <themeContext.Provider value={{ isDarkMode, changeTheme }}>
+      <Customizer
+        settings={customizations?.settings}
+        scopedSettings={customizations?.scopedSettings}
+      >
+        {children}
+      </Customizer>
+    </themeContext.Provider>
+  );
+};
+
+/** Hook that returns a function that toggles dark mode on or off
+ *
+ * @type {()=>ThemeData}
+ */
+export const useThemeData = () => {
+  const ctx = useContext(themeContext);
+  if (ctx === undefined) {
+    throw new Error("useThemeData must be used within a ThemeProvider");
+  }
+  return ctx;
+};
+
+/**
+ * @typedef ThemeData
+ * @property {()=>void} [changeTheme]
+ * @property {boolean} [isDarkMode]
+ */
