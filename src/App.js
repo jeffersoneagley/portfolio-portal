4f1dce675584ad39076f8e0a36c833e3c76bae61
Ilithor <daryk.kohler@gmail.com>
2020-06-08 11:05:22 -0700

    Implemented routing management

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 import { Navbar } from "./components/Navbar";
+import { Resume } from "./components/Resume";
+import { SocMon } from "./components/SocMon";
 
 /** Renders the portfolio portal
  *
  * @returns {React.FunctionComponent}
  */
 export const App = () => (
-  <div id="App">
-    <h1>
-      <strong>Daryk Kohler</strong>
-    </h1>
-    <Navbar />
-  </div>
+  <Router>
+    <div id="App">
+      <h1>
+        <strong>Daryk Kohler</strong>
+      </h1>
+      <Navbar />
+      <Switch>
+        <Route path="/resume" component={Resume} />
+        <Route path="/socmon" component={SocMon} />
+      </Switch>
+    </div>
+  </Router>
 );
