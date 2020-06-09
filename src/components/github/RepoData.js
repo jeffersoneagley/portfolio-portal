3ad484ae63cdb6caae74646349d617ba6e02e584
Ilithor <daryk.kohler@gmail.com>
2020-06-09 13:13:39 -0700

    Added more repo data

--- src/components/github/RepoData.js
+++ src/components/github/RepoData.js
@@ -13,6 +13,20 @@ export const repoData = [
     content:
       "Dalmatian ICS is the continuation of BC_ICS, an Incident Command System form editor and manager. Originally designed for Eastside Fire & Rescue in Washington State, the utility of Dalmatian ICS extends to any group who rely on the creation and distribution of FEMA's ICS forms.",
     language: "JavaScript, TypeScript, Electron"
+  },
+  {
+    title: "DatingApp",
+    link: "https://github.com/Ilithor/DatingApp",
+    content:
+      "Developed a social platform that allows a user to create a profile, upload a picture, be able to navigate through a list of other user profiles, add a profile to a “like” list and send private messages to a profile of the user’s choosing.",
+    language: "C#, TypeScript, JavaScript"
+  },
+  {
+    title: "VideoStreamingApp",
+    link: "https://github.com/Ilithor/VideoStreamingApp",
+    content:
+      "Developed a streaming platform that allows a user to create a streaming page, where they can link their streaming application, such as OBS, to the page and stream their content. It also allows the user to edit the title and description of the stream, as well as delete it from the server. Makes use of Google authentication.",
+    language: "JavaScript"
   }
 ];
 /**
