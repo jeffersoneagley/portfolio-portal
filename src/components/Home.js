0a858723d12e4cef2821d2d1d8a6b697e4cccccb
Ilithor <daryk.kohler@gmail.com>
2020-06-13 00:21:06 -0700

    Refactor and styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,18 +1,5 @@
 import React from "react";
 
 export const Home = () => {
-  return (
-    <div>
-      <h2>
-        <i>ATTENTION:</i>
-      </h2>
-      <strong>
-        This website is currently being hosted on the Azure Static Apps
-        platform, which is still fairly new on the Azure portal. At this time, I
-        have yet to find a solution that allows frontend routing and access to
-        images in the public folder. All other functionalities works as
-        intended.{" "}
-      </strong>
-    </div>
-  );
+  return <div className="homeNotice"></div>;
 };
